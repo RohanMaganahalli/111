2999f000c6e2d35c02369c4ffe75b69a2a442f17
RohanMaganahalli <rohan7@techie.com>
2023-08-16 20:01:32 +0530

    query_params

--- Backend/shopping-cart-app/public/scripts.js
+++ Backend/shopping-cart-app/public/scripts.js
@@ -1,9 +1,46 @@
 const productListDiv=document.getElementById('product-list');
 const cartDiv=document.getElementById('cart');
 
+function deleteCartItem(product_id){
+    const user_id=1;
+    fetch('delete_from_cart',{
+        method:'POST',
+        headers:{
+            'Content-Type':'application/json',
+        },
+        body:JSON.stringify({user_id,product_id}),
+    })
+    .then(response=>response.json())
+    .then(data=>{
+        alert(data.message);
+        updateCart();
+    })
+    .catch(error=>console.error('Error deleting the product from cart:',error));
+}
+
+function editCartItem(product_id){
+    const newPrice=parseFloat(prompt('Enter the new price:'));
+    if(!isNaN(newPrice)){
+        const user_id=1;
+        fetch('/edit_cart_item',{
+            method:'POST',
+            headers:{
+                'Content-Type':'application/json',
+            },
+            body:JSON.stringify({user_id,product_id,newPrice}),
+        })
+        .then(response=>response.json())
+        .then(data=>{
+            alert(data.message);
+            updateCart();
+        })
+.catch(error=>console.error('Error editing cart item:',error));
+    }
+}
+
 function fetchProducts(){
     fetch('/products')
-    .then(response=>respomse.json())
+    .then(response=>response.json())
     .then(products=>{
         products.forEach(product=>{
             const productCard=document.createElement('div');
@@ -39,7 +76,10 @@ function updateCart(){
         cart_items.forEach(item=>{
             const cartItem=document.createElement('div');
             cartItem.classList.add('cart-item');
-            cartItem.innerHTML=`<p>${item.name}-$${item.price.toFixed(2)}</p>`;
+            cartItem.innerHTML=`<p>${item.name}-$${item.price.toFixed(2)}</p>
+            <button onclick="editCartItem(${item.id})">Edit</button>
+            <button onclick="deleteCartItem(${item.id})">Delete</button>`;
+
             cartDiv.appendChild(cartItem);
         });
     })
--- Backend/shopping-cart-app/src/server.js
+++ Backend/shopping-cart-app/src/server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const cors=require('cors');
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 const products = [
 { id: 1, name: 'Product 1', price: 10.0 },
@@ -13,6 +14,36 @@ const products = [
 const carts = {}; // User ID -> List of product IDs
 
 app.use(express.static('public'));
+//to delete product
+app.post('/delete_from_cart',(req,res)=>{
+    const{user_id,product_id}=req.body;
+    if(!user_id||!product_id){
+        return res.status(400).json({error:'Invalid request'});
+    }
+    if(carts[user_id]){
+        carts[user_id]=carts[user_id].filter(id=>id!==product_id);
+        return res.status(200).json({message:'Product deleted from the cart'});
+    }else{
+        return res.status(200).json({message:'Cart is empty'});
+    }
+});
+
+//to edit product
+app.post('/edit_cart_item',(req,res)=>{
+    const {user_id,product_id,newPrice}=req.body;
+    if(!user_id||!product_id||isNaN(newPrice)){
+        return res.status(400).json({error:'Invalid request'});
+    }
+    if(carts[user_id]&&carts[user_id].inculde(product_id)){
+        const productToUpdate=products.find(product=>product.id===product_id);
+        if(productToUpdate){
+            productToUpdate.price=newPrice;
+            return res.status(200).json({message:'Cart item price updated'});
+        }else{
+            return res.status(404).json({error:'Product not found'});
+        }
+    }
+});
 
 // Endpoint to fetch products
 app.get('/products', (req, res) => {
@@ -49,6 +80,6 @@ return products.find(product => product.id === productId);
 res.json(cart_items);
 });
 
-app.listen(5555, () => {
-console.log('Server is running on port 5555');
+app.listen(5559, () => {
+console.log('Server is running on port 5559');
 });
